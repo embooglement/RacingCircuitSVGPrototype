55d9ca0bababaacd6b5573e8864f5e0898f11b2f
Kevin Beason <kevin.beason@addepar.com>
2020-08-18 11:36:26 -0400

    Refactor structure

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ function rand(min, max) {
   return (Math.random() * (max - min)) + min;
 }
 
-function generateNewCircuitSVG() {
+function getSVG() {
   const svg = document.createElementNS(NS, 'svg');
   svg.setAttribute('xmlns', NS);
   svg.setAttribute('viewBox', '0 0 1 1');
   svg.setAttribute('width', 1);
   svg.setAttribute('height', 1);
+  return svg;
+}
 
+function getCircuit() {
   const circle = document.createElementNS(NS, 'circle');
   circle.setAttribute('cx', 0.5);
   circle.setAttribute('cy', 0.5);
   circle.setAttribute('r', rand(0.15, 0.4));
-  circle.setAttribute('stroke', 'purple');
+  circle.setAttribute('stroke', 'black');
   circle.setAttribute('stroke-width', 0.01);
   circle.setAttribute('fill', 'transparent');
 
-  svg.appendChild(circle);
+  return circle;
+}
 
+function generateNewCircuitSVG() {
+  const svg = getSVG();
+  const circuit = getCircuit();
+  svg.appendChild(circuit);
   return svg;
 }
 
