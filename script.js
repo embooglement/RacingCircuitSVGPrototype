39a2b6b269a0ccc2ec5d747b4e403eac1e8dde22
Kevin Beason <kevin.beason@addepar.com>
2020-08-18 13:33:36 -0400

    Elliptical arcs, highlight points

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let numPoints = 0;
 let minRadius = 0;
 let maxRadius = 0;
 
+// TODO(kevin): maybe remove this
+let arcRadius = 0;
+
 function getNumPoints() {
   return numPoints;
 }
@@ -11,12 +14,40 @@ function getRadius() {
   return rand(minRadius, maxRadius);
 }
 
+function getArcRadius() {
+  return arcRadius;
+}
+
 function rand(min, max) {
   return (Math.random() * (max - min)) + min;
 }
 
 function pt(point) {
   return '' + point.x + ',' + point.y;
+}
+
+function distance(point1, point2) {
+  return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
+}
+
+function toDegrees(radians) {
+  return radians * 180 / Math.PI;
+}
+
+function clamp(value, min, max) {
+  if (value < min) return min;
+  else if (value > max) return max;
+  else return value;
+}
+
+function buildCommand(command, ...args) {
+  let c = ' ' + command;
+  for (const arg of args) {
+    c += ' ';
+    c += arg;
+  }
+
+  return c;
 }
 
 function getSVGElement() {
@@ -32,6 +63,7 @@ function getPathElement() {
   const path = document.createElementNS(NS, 'path');
   path.setAttribute('stroke', 'black');
   path.setAttribute('stroke-width', 0.008);
+  path.setAttribute('stroke-linejoin', 'round');
   path.setAttribute('fill', 'transparent');
   return path;
 }
@@ -54,38 +86,66 @@ function getPathPoints() {
   return points;
 }
 
-function getPathCommands() {
-  // const d = `
-  //   M 0.10,0.30
-  //   A .20,.20 0,0,1 .50,.30
-  //   A .20,.20 0,0,1 .90,.30
-  //   Q .90,.60 .50,.90
-  //   Q .10,.60 .10,.30
-  //   z
-  // `;
+function getEllipticalArcCommand(point, prevPoint) {
 
-  let points = getPathPoints();
+  // Whether or not to use the larger elliptical curve between two points. We
+  // never want this turned on because the points are close together and we
+  // want shorter paths connecting them.
+  const largeSweepFlag = 0;
+
+  // Whether or not to use clockwise (0) or counterclockwise (1) arc. The
+  // counterclockwise arcs lead to convex shapes, whereas the clockwise arcs
+  // lead to concave shapes, so we want this flag to be set to counterclockwise.
+  const sweepFlag = 1;
+
+  const angle = Math.atan2(point.y - prevPoint.y, point.x - prevPoint.x);
+  const dist = distance(point, prevPoint);
+  const radii = {
+    x: getArcRadius(),
+    y: dist / 2,
+  };
+
+  return buildCommand('A', pt(radii), toDegrees(angle), largeSweepFlag, sweepFlag, pt(point));
+}
+
+function getPathCommands(points) {
   const firstPoint = points[0];
   points = points.slice(1);
 
-  let d = 'M ';
-  d += pt(firstPoint);
-
+  let commands = [buildCommand('M', pt(firstPoint))];
+  let prevPoint = firstPoint;
   for (const point of points) {
-    d += ' L';
-    d += pt(point);
+    // TODO(kevin): dynamically choose how to connect two points? i.e. use
+    // elliptical arcs AND bezier curves.
+    const command = getEllipticalArcCommand(point, prevPoint);
+    commands.push(command);
+    prevPoint = point;
   }
 
-  d += ' z';
-
-  return d;
+  commands.push(buildCommand('z'));
+  return commands.join(' ');
 }
 
 function getCircuitElement() {
-  const path = getPathElement();
-  path.setAttribute('d', getPathCommands());
-  return path;
+  const g = document.createElementNS(NS, 'g');
+  const points = getPathPoints();
 
+  const path = getPathElement(points);
+  path.setAttribute('d', getPathCommands(points));
+  g.appendChild(path);
+
+
+  for (const point of points) {
+    const circle = document.createElementNS(NS, 'circle');
+    circle.setAttribute('cx', point.x);
+    circle.setAttribute('cy', point.y);
+    circle.setAttribute('r', 0.01);
+    circle.setAttribute('fill', 'red');
+
+    g.appendChild(circle);
+  }
+
+  return g;
 }
 
 function generateNewCircuitSVG() {
@@ -99,28 +159,27 @@ function initializeInputs() {
   const numPointsInput = document.getElementById('num-points');
   const minRadiusInput = document.getElementById('min-radius');
   const maxRadiusInput = document.getElementById('max-radius');
+  const arcRadiusInput = document.getElementById('arc-radius');
 
   numPoints = parseInt(numPointsInput.value, 10);
   minRadius = parseFloat(minRadiusInput.value);
   maxRadius = parseFloat(maxRadiusInput.value);
-
-  console.log("numPoints is", numPoints);
-  console.log("minRadius is", minRadius);
-  console.log("maxRadius is", maxRadius);
+  arcRadius = parseFloat(arcRadiusInput.value);
 
   numPointsInput.addEventListener('change', (e) => {
     numPoints = parseInt(numPointsInput.value, 10);
-    console.log('numPoints is now', numPoints);
   });
 
   minRadiusInput.addEventListener('change', (e) => {
     minRadius = parseFloat(minRadiusInput.value);
-    console.log('minRadius is now', minRadius);
   });
 
   maxRadiusInput.addEventListener('change', (e) => {
     maxRadius = parseFloat(maxRadiusInput.value);
-    console.log('maxRadius is now', maxRadius);
+  });
+
+  arcRadiusInput.addEventListener('change', (e) => {
+    arcRadius = parseFloat(arcRadiusInput.value);
   });
 }
 
